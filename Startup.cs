e92eec59548663d8e0246a9bfdaf37341ffd8ecd
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2024-01-17 10:12:26 +0000

    add Option to see if that overrides the Elastic APM

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using Elastic.Apm.Azure.Functions;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 [assembly: FunctionsStartup(typeof(Sen.InProcess.Startup))]
 
 namespace Sen.InProcess;
 
+public class Options
+{
+}
+
 internal class Startup : FunctionsStartup
 {
-    public override void Configure(IFunctionsHostBuilder builder) => builder.AddElasticApm();
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddOptions<Options>()
+            .Configure<IConfiguration>((settings, configuration) =>
+            {
+                configuration.GetSection("Options").Bind(settings);
+            });
+
+        builder.AddElasticApm();
+    }
 }
