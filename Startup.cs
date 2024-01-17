c1fee19f79280e5b8feef85ce5e4509ce81ec066
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2024-01-17 09:02:48 +0000

    in process function app

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Elastic.Apm.Azure.Functions;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Sen.InProcess.Startup))]
+
+namespace Sen.InProcess;
+
+internal class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder) => builder.AddElasticApm();
+}
