006e4551d00cf58ba21ebbded9c6e40b380f662c
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2024-01-19 12:17:24 +0000

    updating startup

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using Elastic.Apm.Azure.Functions;
+﻿using Elastic.Apm.Azure.Functions;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 
 [assembly: FunctionsStartup(typeof(Sen.InProcess.Startup))]
 
@@ -17,12 +13,6 @@ internal class Startup : FunctionsStartup
 {
     public override void Configure(IFunctionsHostBuilder builder)
     {
-        builder.Services.AddOptions<Options>()
-            .Configure<IConfiguration>((settings, configuration) =>
-            {
-                configuration.GetSection("Options").Bind(settings);
-            });
-
         builder.AddElasticApm();
     }
 }
