0daf1c13955483ab7a9f79fb95bbc283541a1b82
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2024-01-19 10:17:40 +0000

    Revert "adding in ping check to tsdfdfsfsdkljdfs"
    This reverts commit 3727316da51d96e3ecb972804ce4f2058b1a6637.

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Net;
-using System.Net.NetworkInformation;
 using Elastic.Apm.Azure.Functions;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
@@ -11,34 +9,19 @@ using Microsoft.Extensions.Options;
 
 namespace Sen.InProcess;
 
+public class Options
+{
+}
+
 internal class Startup : FunctionsStartup
 {
-    public static string CanPingElasticServer;
-
-    static string PingUrl(string url)
-    {
-        try
-        {
-            Ping ping = new Ping();
-            IPAddress ipAddress = null;
-            bool isValidIp = System.Net.IPAddress.TryParse(url, out ipAddress);
-
-            PingReply reply = isValidIp ? ping.Send(url) : ping.Send(new Uri(url).Host);
-
-            return $"Ping succeeded:{reply.Status.ToString()}";
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An error occurred: {ex.Message}");
-            return $"An error occurred: {ex.Message}";
-        }
-    }
-
     public override void Configure(IFunctionsHostBuilder builder)
     {
-        string elasticAddress = Environment.GetEnvironmentVariable("ELASTIC_APM_SERVER_URL") ??
-                                "https://ukho-nonlive-elastic.apm.uksouth.azure.elastic-cloud.com";
-        CanPingElasticServer = PingUrl(elasticAddress);
+        builder.Services.AddOptions<Options>()
+            .Configure<IConfiguration>((settings, configuration) =>
+            {
+                configuration.GetSection("Options").Bind(settings);
+            });
 
         builder.AddElasticApm();
     }
